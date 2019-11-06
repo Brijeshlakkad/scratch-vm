686f1faa05646c5453533cca77d129cb18be4c7a
Brijeshlakkad <brijeshlakkad22@gmail.com>
2019-11-06 16:11:03 +0530

    Square block added

--- src/blocks/scratch3_operators.js
+++ src/blocks/scratch3_operators.js
@@ -33,7 +33,8 @@ class Scratch3OperatorsBlocks {
             operator_contains: this.contains,
             operator_mod: this.mod,
             operator_round: this.round,
-            operator_mathop: this.mathop
+            operator_mathop: this.mathop,
+            operator_square: this.square
         };
     }
 
@@ -148,6 +149,10 @@ class Scratch3OperatorsBlocks {
         case '10 ^': return Math.pow(10, n);
         }
         return 0;
+    }
+
+    square(args){
+      return Cast.toNumber(args.NUM) * Cast.toNumber(args.NUM);
     }
 }
 
